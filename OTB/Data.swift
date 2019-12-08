37e0e8d87592ada5cac41a0ab75fbd697d588db0
Joe <j.s@joeis.us>
2019-12-07 21:38:36 -0500

    Data
    Fixed updates

---
+++ OTB/Data.swift
@@ -0,0 +1,54 @@
+////  Data.swift
+//  OTB
+//  Created on: 12/4/19
+//  Using Swift 5.0
+//  Copyright ©  2019 All rights reserved.
+//
+
+import Foundation
+// MARK: SHARE TIME 
+
+let SHARE_0 = "I have __DAYS__ #OTBAPP 🥳"
+let SHARE_1 = "I should start packing with __DAYS__ left! 😅"
+let SHARE_2 = "I Should stop bragging __DAYS__ left 😎"
+let SHARE_3 = "Dont fuck it up __DAYS__ left 🙈 "
+let SHARE_4 = "Time to partayyyy 😎 __DAYS__ #OTBAPP"
+let SHARE_5 = "Joe, Stop telling people how many days are left. They already want you to leave. __DAYS__ #OTBAPP"
+
+// MARK: QUOTES FOR MAIN VIEW
+let QUOTE_0 = "\"Put something inspirational here\""
+let QUOTE_1 = "\"Dont Fuck it up - Logan 2019 - Joe 2019\""
+let QUOTE_2 = "I think it’s great that they’re doing this, and I hope it works out."
+let QUOTE_3 = "Most important, have the courage to follow your heart and intuition."
+let QUOTE_4 = "\"Be proud of yourself. Do the best work of your life.\""
+let QUOTE_5 = "\"Stop looking for a countdown. Look at the road ahead.\""
+
+
+// MARK: DECLARE ARRAY OF DATA
+let QUARRAY = [
+    QUOTE_0,
+    QUOTE_1,
+    QUOTE_2,
+    QUOTE_3,
+    QUOTE_4,
+    QUOTE_5
+]
+
+let SHARRAY = [
+    SHARE_0,
+    SHARE_1,
+    SHARE_2,
+    SHARE_3,
+    SHARE_4,
+    SHARE_5
+]
+
+
+let ImageArray = [
+    "bg-0.jpeg",
+    "bg-1.jpeg",
+    "bg-2.jpeg",
+    "bg-3.jpeg",
+    "bg-4.jpeg",
+]
+
--- OTB/ViewController.swift
+++ OTB/ViewController.swift
@@ -8,6 +8,8 @@ import Foundation
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
     @IBOutlet weak var dayLabel: UILabel!
     @IBOutlet weak var hrLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
@@ -15,13 +17,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var platterView: UIView!
     @IBOutlet weak var backgroundImageView: UIImageView!
-    let ImageArray = [
-        "bg-0.jpeg",
-        "bg-1.jpeg",
-        "bg-2.jpeg",
-        "bg-3.jpeg",
-        "bg-4.jpeg",
-    ]
+    @IBOutlet weak var inspirationLabel: UILabel!
+    
 
     override func viewDidLoad() {
         firstLaunch()
@@ -33,31 +30,48 @@ class ViewController: UIViewController {
             imageView.image = img
             
         } else {
-            return
+            return 
         }
-
+        
         platterView.layer.cornerRadius = 10
         platterView.clipsToBounds = true
         dayLabel.font = .boldSystemFont(ofSize: 47)
-
+        
     super.viewDidLoad()
-
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
         updateTimer()
+        inspirationLabel.text = self.randomText()
         backgroundImageView.image = self.randomImage()
         
-
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
          navigationController?.setNavigationBarHidden(false, animated: animated)
     }
-
-
-    
+// MARK: First Launch
+    func firstLaunch(){
+        let launchedBefore = UserDefaults.standard.bool(forKey: "launchednever")
+        if launchedBefore {
+            print("Not First Launch")
+        } else {
+            let strDate = "01-01-2020"
+            let result = strDate.split(separator: "-")
+            let defaults = UserDefaults.standard
+            UserDefaults.standard.set(String(result[0]), forKey: "day")
+            UserDefaults.standard.set(String(result[1]), forKey: "month")
+            UserDefaults.standard.set(String(result[2]), forKey: "year")
+            defaults.synchronize()
+            UserDefaults.standard.set(true, forKey: "launchednever")
+            
+            //display UI alert Giving option to set First Coundown timer
+            
+        }
+    }
+// MARK: Update Timer
     func updateTimer() {
         let date = NSDate()
         let calendar = Calendar.current
@@ -77,38 +91,36 @@ class ViewController: UIViewController {
         competitionDate.year = yearint!
         competitionDate.month = monthint!
         competitionDate.day = dayint!
-        
         competitionDate.hour = 00
         competitionDate.minute = 00
         let competitionDay = userCalendar.date(from: competitionDate as DateComponents)!
-        //
         let CompetitionDayDifference = calendar.dateComponents([.day, .hour, .minute], from: currentDate!, to: competitionDay)
-        //
         let daysLeft = CompetitionDayDifference.day
         let hoursLeft = CompetitionDayDifference.hour
         let minutesLeft = CompetitionDayDifference.minute
-        //
         dayLabel.text = "\(daysLeft ?? 0 ) Days"
         hrLabel.text = "\(hoursLeft ?? 0) Hours"
         minLabel.text = "\(minutesLeft ?? 0) Minutes"
         
     }
+    
+    
+    // MARK: Button Actions
+    
+    
     @IBAction func editButton(_ sender: Any) {
         let controller = storyboard?.instantiateViewController(withIdentifier: "DatePicker")
         self.navigationController!.pushViewController(controller!, animated: true)
         
     }
-    func randomImage() -> UIImage {
-        let unsignedArrayCount = UInt32(ImageArray.count)
-        let unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
-        let randomNumber = Int(unsignedRandomNumber)
-        return UIImage(named: ImageArray[randomNumber])!
-    }
 
     @IBAction func shareButtonClicked(sender: AnyObject)
       {
           //Set the default sharing message.
-        let message = "OTB reports I have  \(dayLabel.text ?? "UNKNOWN") left"
+        
+        let data = self.randomShare()
+        let message = data.replacingOccurrences(of: "__DAYS__", with: "\(dayLabel.text ?? "UNKNOWN")")
+        
         // Add array and radomize the message
           //Set the link to share.
           if let link = NSURL(string: "http://www.joeis.us")
@@ -120,23 +132,22 @@ class ViewController: UIViewController {
             self.present(activityVC, animated: true, completion: nil)
           }
       }
-    func firstLaunch(){
-        let launchedBefore = UserDefaults.standard.bool(forKey: "launchednever")
-        if launchedBefore {
-            print("Not First Launch")
-        } else {
-            let strDate = "01-01-2020"
-            let result = strDate.split(separator: "-")
-            let defaults = UserDefaults.standard
-            UserDefaults.standard.set(String(result[0]), forKey: "day")
-            UserDefaults.standard.set(String(result[1]), forKey: "month")
-            UserDefaults.standard.set(String(result[2]), forKey: "year")
-            defaults.synchronize()
-            UserDefaults.standard.set(true, forKey: "launchednever")
-            
-            //display UI alert Giving option to set First Coundown timer
-            
-        }
+    // MARK: Logic
+    
+    func randomImage() -> UIImage {
+        let unsignedArrayCount = UInt32(ImageArray.count)
+        let unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
+        let randomNumber = Int(unsignedRandomNumber)
+        return UIImage(named: ImageArray[randomNumber])!
+    }
+    
+
+    func randomText() -> String {
+        return QUARRAY[Int(arc4random_uniform(UInt32(QUARRAY.count)))]
+    }
+    
+    func randomShare() -> String {
+        return SHARRAY[Int(arc4random_uniform(UInt32(SHARRAY.count)))]
     }
     
 }
